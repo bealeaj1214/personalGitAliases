2a6f56e3f1123e496cf3b851812c7c200243d389
Alan Beale <alan.beale@chicagotrading.com>
2011-08-11 15:12:31 -0500

    updated to real version

--- bin/gitAppend.sh
+++ bin/gitAppend.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 OLD_HEAD=$(git rev-parse HEAD) && \
-    echo git reset --soft HEAD^ && \
-    echo git add -- "$@" && \
-    echo git commit -c ${OLD_HEAD}
+    git reset --soft HEAD^ && \
+    git add -- "$@" && \
+    git commit -c ${OLD_HEAD}
+
