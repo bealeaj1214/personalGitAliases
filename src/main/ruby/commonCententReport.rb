f607bae5b85c120b7df48b222efeea6f51efe77e
Alan Beale <alan.beale@chicagotrading.com>
2011-08-27 14:49:36 -0500

    Added functions to check that branch specification is valid before proceeding.

--- src/main/ruby/commonCententReport.rb
+++ src/main/ruby/commonCententReport.rb
@@ -6,6 +6,30 @@ def check_usage
   end
 end
 
+def reportInvalidBranchSpec(check_value,branch_value)
+  unless check_value 
+    puts " invalid git branch specification :#{branch_value}"
+  end
+end
+
+def isValidCommit(branch) 
+  command = "git rev-parse --verify #{branch} >/dev/null  2>1"
+  junk= %x(#{command})
+  return ($?.to_i == 0)
+end
+
+def check_branches(branch_alpha, branch_beta)
+  checkAlpha = isValidCommit(branch_alpha)
+  checkBeta = isValidCommit(branch_beta)
+  
+  reportInvalidBranchSpec(checkAlpha,branch_alpha)
+  reportInvalidBranchSpec(checkBeta ,branch_beta)
+
+  unless checkAlpha && checkBeta
+    exit
+  end
+end
+
 def identify_common_commits(branch_alpha, branch_beta)
   command = "git cherry #{branch_alpha}  #{branch_beta} | awk ' $1~/-/ { print $2} '"
 
@@ -15,6 +39,7 @@ end
 
 if $0 == __FILE__
   check_usage
+  check_branches(ARGV[0], ARGV[1])
   identify_common_commits(ARGV[0], ARGV[1])
 end
 
