a8021e32b6a6d1527ee9e0132645bd6da478fe47
Alan Beale <alan.beale@chicagotrading.com>
2011-08-27 16:30:36 -0500

    Working initial version of common content report

--- src/main/ruby/commonCententReport.rb
+++ src/main/ruby/commonCententReport.rb
@@ -25,6 +25,8 @@ def check_branches(branch_alpha, branch_beta)
   reportInvalidBranchSpec(checkAlpha,branch_alpha)
   reportInvalidBranchSpec(checkBeta ,branch_beta)
 
+  #TODO  Check that the branches don't point to the same commit
+  #TODO  Check that neither branch is an immediate ancestor of the other
   unless checkAlpha && checkBeta
     exit
   end
@@ -46,33 +48,60 @@ def identify_common_commits(branch_alpha, branch_beta)
 
 end
 
-def processCommits(commits)
-  commits.each { |commit|
-    command = " git show #{commit} | git patch-id "
-    cmd_result = %x[#{command}]
 
-    if ($?.to_i == 0)
-      parts=cmd_result.split
-      if (parts.length == 2) 
-        puts " patch SHA1: #{parts[0]}  commit SHA1 : #{parts[1]} "
-      end
-    end
+def subjectReports(commits)
+  
+  commits.each { |commit|
+    command = "GIT_PAGER= git log -1 --pretty='%h %s'  #{commit}"
+    cmd_result =%x(#{command})
+    puts "\t#{cmd_result}"
   }
+
 end
 
 class CommonContentReport
   def initialize(branch_alpha, branch_beta)
     @branch1 =branch_alpha
     @branch2 =branch_beta
-    @common_patches = Hash.new([])
+    @common_patches = Hash.new{|h,k| h[k] = []}
   end
 
+  def processCommits(commits)
+    commits.each { |commit|
+      command = " git show #{commit} | git patch-id "
+      cmd_result = %x[#{command}]
+      
+      if ($?.to_i == 0)
+        parts=cmd_result.split
+        if (parts.length == 2) 
+          @common_patches[parts[0]] =  @common_patches[parts[0]] + [parts[1]]
+        end
+      end
+    }
+  end
+  
+
+  def report 
+    puts " number of common patches #{@common_patches.length} "
+    @common_patches.each { |key,commits|
+      puts " #{key} "
+      subjectReports(commits)
+    }
+  end
+
+  def process
+    commits1 =identify_common_commits(@branch1, @branch2)
+    processCommits(commits1)
+    commits2 =identify_common_commits(@branch2, @branch1)
+    processCommits(commits2)
+  end
 end
 
 if $0 == __FILE__
   check_usage
   check_branches(ARGV[0], ARGV[1])
-  test =identify_common_commits(ARGV[0], ARGV[1])
-  processCommits(test)
+  ccReport =CommonContentReport.new(ARGV[0], ARGV[1])
+  ccReport.process
+  ccReport.report
 end
 
