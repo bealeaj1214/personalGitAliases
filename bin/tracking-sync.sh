c699a77c6876f51d1134c7028dc027a4b8327b80
Alan Beale <alan.beale@chicagotrading.com>
2011-09-26 08:47:15 -0500

    Added support to remember where you started

--- bin/tracking-sync.sh
+++ bin/tracking-sync.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+ORG_BRANCH=$(git status -b -s | awk ' /##/ {print $NF;} ' )
 # hillbilly style script for sync  of tracking branches ( git to git remote sync op)
 for BRANCH in $( git branch | awk ' $NF~/^tracking\// {print $NF} ' | xargs); do 
     git checkout $BRANCH && git pull
 done
+git checkout ${ORG_BRANCH}
 
 # Improvements
   # remember the branch that you started on and fast fail if there unstored changes
