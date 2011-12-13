9d4b1c0c0459cea5abab4ef8964b8e9eb18a9fd6
Alan Beale <alan.beale@chicagotrading.com>
2011-12-13 15:22:42 -0600

    add support for git svn fetch call

--- bin/gateway-sync.sh
+++ bin/gateway-sync.sh
@@ -6,8 +6,9 @@ for BRANCH in $( git branch | awk ' $NF~/^active\// {print $NF} ' | xargs); do
     git checkout $BRANCH && git svn rebase 
 done
 
+git svn fetch
+
 git checkout ${ORG_BRANCH}
 # Improvements
-  # remember the branch that you started on 
   # fast fail if there unstored changes
   #  Collect which branches had errors and report
