8efabffc71556bb5fb0afe151a6d8138164efaea
Alan Beale <alan.beale@chicagotrading.com>
2012-04-12 10:52:43 -0500

    Updated and improved tracking-sync  improved scripting to extract brnch name of current HEAD

--- bin/tracking-sync.sh
+++ bin/tracking-sync.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-ORG_BRANCH=$(git status -b -s | awk ' /##/ {print $NF;} ' )
+ORG_BRANCH="$(git symbolic-ref HEAD 2>/dev/null)" || exit 1
+ORG_BRANCH=${ORG_BRANCH##refs/heads/}
+
+#echo "saving branch ${ORG_BRANCH} "
+
 # hillbilly style script for sync  of tracking branches ( git to git remote sync op)
 for BRANCH in $( git branch | awk ' $NF~/^tracking\// {print $NF} ' | xargs); do 
     git checkout $BRANCH && git pull
 done
+
+#echo "switching back to ${ORG_BRANCH} "
+
 git checkout ${ORG_BRANCH}
 
 # Improvements
